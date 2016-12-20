e32a5f464c01d0e80874ab4ed7f59e87c4be032f
Luke Easton <Luke.Easton@landregistry.gov.uk>
2016-12-20 10:50:28 +0000

    re-organising routing

--- application/views.py
+++ application/views.py
@@ -3,15 +3,11 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
 @app.route('/layout')
 def layout():
     return render_template('layout.html')
 
-@app.route('/Homepage')
+@app.route('/')
 def homepage():
     return render_template('Homepage.html')
 
