572bb9205631c761c6238971bd64c96569353877
Pirie <Alexander.Pirie@landregistry.gov.uk>
2016-12-21 12:12:29 +0000

    Added page title attribute

--- application/views.py
+++ application/views.py
@@ -13,23 +13,23 @@ def homepage():
 
 @app.route('/Python')
 def python():
-    return render_template('Python.html')
+    return render_template('Python.html', title="Python Page")
 
 @app.route('/SQL')
 def SQL():
-    return render_template('SQL.html')
+    return render_template('SQL.html', title="SQL Page")
 
 @app.route('/HTML')
 def html():
-    return render_template('html_css_page_INTRO.html')
+    return render_template('html_css_page_INTRO.html', title="HTML & CSS Intro")
 
 @app.route('/HTML-Content')
 def htmlcontent():
-    return render_template('html_css_page_html_content.html')
+    return render_template('html_css_page_html_content.html', title="HTML Page")
 
 @app.route('/HTML-CSS')
 def htmlcss():
-    return render_template('html_css_page_css_content.html')
+    return render_template('html_css_page_css_content.html', title="CSS Page")
 
 @app.route('/Git')
 def git():
@@ -37,15 +37,15 @@ def git():
 
 @app.route('/Java')
 def java():
-    return render_template('Java.html')
+    return render_template('Java.html', title="Java Page")
 
 @app.route('/Personal-Experiences')
 def personalexperiences():
-    return render_template('PE.html')
+    return render_template('PE.html', title="Personal-Experiences Page")
 
 @app.route('/Links')
 def links():
-    return render_template('Links.html')
+    return render_template('Links.html', title="Useful Links Page")
 
 if __name__ == "__main__":
     app.run()
