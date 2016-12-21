446761faef8024156346dcfd673dcc71cb88c56a
Luke Easton <Luke.Easton@landregistry.gov.uk>
2016-12-21 12:19:26 +0000

    added titles -amended.

--- application/views.py
+++ application/views.py
@@ -5,19 +5,19 @@ app.config['DEBUG'] = True
 
 @app.route('/layout')
 def layout():
-    return render_template('layout.html')
+    return render_template('layout.html', title="Layout")
 
 @app.route('/')
 def homepage():
-    return render_template('Homepage.html')
+    return render_template('Homepage.html', title="Home")
 
 @app.route('/Python')
 def python():
-    return render_template('Python.html', title="Python Page")
+    return render_template('Python.html', title="Python")
 
 @app.route('/SQL')
 def SQL():
-    return render_template('SQL.html', title="SQL Page")
+    return render_template('SQL.html', title="SQL")
 
 @app.route('/HTML')
 def html():
@@ -25,27 +25,27 @@ def html():
 
 @app.route('/HTML-Content')
 def htmlcontent():
-    return render_template('html_css_page_html_content.html', title="HTML Page")
+    return render_template('html_css_page_html_content.html', title="HTML")
 
 @app.route('/HTML-CSS')
 def htmlcss():
-    return render_template('html_css_page_css_content.html', title="CSS Page")
+    return render_template('html_css_page_css_content.html', title="CSS")
 
 @app.route('/Git')
 def git():
-    return render_template('Git.html')
+    return render_template('Git.html', title="Git")
 
 @app.route('/Java')
 def java():
-    return render_template('Java.html', title="Java Page")
+    return render_template('Java.html', title="Java")
 
 @app.route('/Personal-Experiences')
 def personalexperiences():
-    return render_template('PE.html', title="Personal-Experiences Page")
+    return render_template('PE.html', title="Personal-Experiences")
 
 @app.route('/Links')
 def links():
-    return render_template('Links.html', title="Useful Links Page")
+    return render_template('Links.html', title="Useful Links")
 
 if __name__ == "__main__":
     app.run()
