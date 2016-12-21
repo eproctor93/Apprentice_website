d1eb05000bea0a130d8a423ea9a0be08a75cc7e7
Luke Easton <Luke.Easton@landregistry.gov.uk>
2016-12-21 12:22:04 +0000

    added titles -amended

--- application/views.py
+++ application/views.py
@@ -9,7 +9,7 @@ def layout():
 
 @app.route('/')
 def homepage():
-    return render_template('Homepage.html', title="Home")
+    return render_template('Homepage.html', title="Homepage")
 
 @app.route('/Python')
 def python():
@@ -21,15 +21,15 @@ def SQL():
 
 @app.route('/HTML')
 def html():
-    return render_template('html_css_page_INTRO.html', title="HTML & CSS Intro")
+    return render_template('html_css_page_INTRO.html', title="HTML & CSS")
 
 @app.route('/HTML-Content')
 def htmlcontent():
-    return render_template('html_css_page_html_content.html', title="HTML")
+    return render_template('html_css_page_html_content.html', title="HTML-Content")
 
 @app.route('/HTML-CSS')
 def htmlcss():
-    return render_template('html_css_page_css_content.html', title="CSS")
+    return render_template('html_css_page_css_content.html', title="HTML-CSS-Content")
 
 @app.route('/Git')
 def git():
@@ -41,7 +41,7 @@ def java():
 
 @app.route('/Personal-Experiences')
 def personalexperiences():
-    return render_template('PE.html', title="Personal-Experiences")
+    return render_template('PE.html', title="Personal Experiences")
 
 @app.route('/Links')
 def links():
