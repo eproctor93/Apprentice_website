8b23d974e95eeaa11a970e92ec46015e813e4506
Luke Easton <Luke.Easton@landregistry.gov.uk>
2017-01-06 10:13:02 +0000

    sorted out styling on my pages, added other peoples content to clash less

--- application/views.py
+++ application/views.py
@@ -15,15 +15,62 @@ def homepage():
 def python():
     return render_template('Python.html')
 
+@app.route('/python/pythonbasics')
+def pythonbasics():
+    return render_template('pythonbasics.html')
+
+@app.route('/python/pyvariables')
+def pyvariables():
+    return render_template('pyvariables.html')
+
+@app.route('/python/pyfunctions')
+def pyfunctions():
+    return render_template('pyfunctions.html')
+
+@app.route('/python/pyselection')
+def pyselection():
+    return render_template('pyselection.html')
+
+@app.route('/python/pyiteration')
+def pyiteration():
+    return render_template('pyiteration.html')
+
+@app.route('/python/pylists')
+def pylists():
+    return render_template('pylists.html')
+
 @app.route('/SQL')
 def SQL():
     return render_template('SQL.html')
+
+@app.route('/SQL/sqlSyntax')
+def sqlSyntax():
+    return render_template('sqlSyntax.html')
+
+@app.route('/SQL/sqlSelect')
+def sqlSelect():
+    return render_template('sqlSelect.html')
+
+@app.route('/SQL/sqlUpdate')
+def sqlUpdate():
+    return render_template('sqlUpdate.html')
+
+@app.route('/SQL/sqlInsert')
+def sqlInsert():
+    return render_template('sqlInsert.html')
+
+@app.route('/SQL/sqlCreate')
+def sqlCreate():
+    return render_template('sqlCreate.html')
+    
+@app.route('/SQL/sqlRelations')
+def sqlRelations():
+    return render_template('sqlRelations.html')
 
 @app.route('/HTML')
 def html():
     title='HTML & CSS Introduction'
     return render_template('html_INTRO.html', pagetitle=title)
-
 
 @app.route('/CSS-Content')
 def csscontent():
@@ -32,8 +79,7 @@ def csscontent():
 
 @app.route('/Git')
 def git():
-
-    return render_template('Git.html', pagetitle='Git Contenth')
+    return render_template('Git.html', pagetitle='Git Content')
 
 @app.route('/Java')
 def java():
