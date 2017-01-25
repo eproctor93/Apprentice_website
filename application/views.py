0308a95e1628a5cd73bf1493062b7de33146f624
Luke Easton <Luke.Easton@landregistry.gov.uk>
2017-01-25 11:11:41 +0000

    added titles and styling to pages

--- application/views.py
+++ application/views.py
@@ -4,19 +4,19 @@ app = Flask(__name__)
 
 @app.route('/layout')
 def layout():
-    return render_template('layout.html')
+    return render_template('layout.html', pagetitle='LAYOUT')
 
 @app.route('/')
 def homepage():
-    return render_template('Homepage.html')
+    return render_template('Homepage.html', pagetitle='IT Solutions Apprenticeship')
 
 @app.route('/Python')
 def python():
-    return render_template('Python.html')
+    return render_template('Python.html', pagetitle='Python')
 
 @app.route('/SQL')
 def SQL():
-    return render_template('SQL.html')
+    return render_template('SQL.html', pagetitle='SQL')
 
 @app.route('/HTML')
 def html():
@@ -29,15 +29,15 @@ def git():
 
 @app.route('/Java')
 def java():
-     return render_template('Java.html')
+     return render_template('Java.html', pagetitle='Java')
 
 @app.route('/Personal-Experiences')
 def personalexperiences():
-    return render_template('PE.html')
+    return render_template('PE.html', pagetitle='Personal Experiences')
 
 @app.route('/Links')
 def links():
-    return render_template('Links.html')
+    return render_template('Links.html', pagetitle='Useful Links')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
