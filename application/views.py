08ee8dce8f79340be268e98b5a0637a793fc411a
Luke Easton <Luke.Easton@landregistry.gov.uk>
2017-01-31 13:56:11 +0000

    Fixed linting

--- application/views.py
+++ application/views.py
@@ -1,43 +1,53 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
 
 @app.route('/layout')
 def layout():
     return render_template('layout.html', pagetitle='LAYOUT')
 
+
 @app.route('/')
 def homepage():
     return render_template('Homepage.html', pagetitle='IT Solutions Apprenticeship')
+
 
 @app.route('/Python')
 def python():
     return render_template('Python.html', pagetitle='Python')
 
+
 @app.route('/SQL')
 def SQL():
     return render_template('SQL.html', pagetitle='SQL')
 
+
 @app.route('/HTML')
 def html():
-    title='HTML & CSS Introduction'
+    title = 'HTML & CSS Introduction'
     return render_template('HTML-CSS.html', pagetitle=title)
+
 
 @app.route('/Git')
 def git():
     return render_template('Git.html', pagetitle='Git Content')
 
+
 @app.route('/Java')
 def java():
-     return render_template('Java.html', pagetitle='Java')
+    return render_template('Java.html', pagetitle='Java')
+
 
 @app.route('/Personal-Experiences')
 def personalexperiences():
     return render_template('PE.html', pagetitle='Personal Experiences')
 
+
 @app.route('/Links')
 def links():
     return render_template('Links.html', pagetitle='Useful Links')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
